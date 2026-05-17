4b0df53c53dbd1249fcc06b02335d0e5577e1c12
tamaygz <forevor@gmail.com>
2026-05-17 07:16:57 +0200

    feat(tests): add aiohttp error stubs for improved test coverage

--- tests/conftest.py
+++ tests/conftest.py
@@ -83,5 +83,19 @@ _stub(
 )
 _stub("homeassistant.config_entries", ConfigEntry=object)
 
+# --- aiohttp ---
+class _AiohttpError(Exception):
+    """Stub base for aiohttp errors."""
+
+
+_stub(
+    "aiohttp",
+    ClientSession=MagicMock,
+    ClientTimeout=MagicMock,
+    ClientError=_AiohttpError,
+    ClientConnectionError=_AiohttpError,
+    ServerTimeoutError=_AiohttpError,
+)
+
 # --- wled (python-wled external package) ---
 _stub("wled", WLED=MagicMock)
